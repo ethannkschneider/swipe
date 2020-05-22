711a84d88f5538e5c311fd7179dcac53fa1e0cda
Ethan Schneider <ethannkschneider@gmail.com>
2020-05-22 15:48:30 -0500

    configure prettier

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('home page');
+});
+
+app.listen(5000, () => {
+    console.log('listening ...');
+});
